b748115ece8c30d1ed6732bfd7f650f2b8069f08
niahamanw <shizxyandy+@gmail.com>
2025-05-12 15:28:10 -0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'mhmodsyt517@gmail.com',
-    password: 'MHMODS517Yt@Hr',
+    email: 'Epay39209@gmail.com',
+    password: 'Harry2008@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
